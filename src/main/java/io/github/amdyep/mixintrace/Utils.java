715fdd0c7c57b5e9197c309abe18a0da3e3bc1f8
AMDYep <amdyep@outlook.com>
2023-11-26 19:23:15 +0800

    lowcode

---
+++ src/main/java/io/github/amdyep/mixintrace/Utils.java
@@ -0,0 +1,61 @@
+package io.github.amdyep.mixintrace;
+
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import org.spongepowered.asm.mixin.transformer.ClassInfo;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Utils {
+    @SuppressWarnings("unchecked")
+    public static void printTrace(StackTraceElement[] stackTrace, StringBuilder builder) {
+        if (stackTrace == null || stackTrace.length == 0) return;
+        builder.append("\nMixins in Stacktrace:");
+        try {
+            List<String> classNames = new ArrayList<>();
+            for (StackTraceElement el : stackTrace) {
+                if (!classNames.contains(el.getClassName())) classNames.add(el.getClassName());
+            }
+            boolean found = false;
+            for (String className : classNames) {
+                ClassInfo classInfo = ClassInfo.fromCache(className);
+                if (classInfo == null) continue;
+                Object mixinInfoSetObject = null;
+                try {
+                    Method getMixins = ClassInfo.class.getDeclaredMethod("getMixins");
+                    getMixins.setAccessible(true);
+                    mixinInfoSetObject = getMixins.invoke(classInfo);
+                } catch (Exception ignored) {
+                }
+                if (mixinInfoSetObject == null) {
+                    try {
+                        Field mixinsField = ClassInfo.class.getDeclaredField("mixins");
+                        mixinsField.setAccessible(true);
+                        mixinInfoSetObject = mixinsField.get(classInfo);
+                    } catch (Exception ignored) {
+                    }
+                }
+                if (mixinInfoSetObject == null) continue;
+                Set<IMixinInfo> mixinInfoSet = (Set<IMixinInfo>) mixinInfoSetObject;
+                if (mixinInfoSet.isEmpty()) continue;
+                builder.append("\n\t");
+                builder.append(className);
+                builder.append(":");
+                for (IMixinInfo info : mixinInfoSet) {
+                    builder.append("\n\t\t");
+                    builder.append(info.getClassName());
+                    builder.append(" (");
+                    builder.append(info.getConfig().getName());
+                    builder.append(")");
+                }
+                found = true;
+            }
+            if (!found) builder.append(" None found");
+        } catch (Exception e) {
+            builder.append(" Failed to find Mixin metadata: ").append(e);
+        }
+    }
+}
---
+++ src/main/java/io/github/amdyep/mixintrace/mixin/MixinCrashReport.java
@@ -0,0 +1,32 @@
+package io.github.amdyep.mixintrace.mixin;
+
+import io.github.amdyep.mixintrace.Utils;
+import net.minecraft.CrashReport;
+import org.apache.commons.lang3.StringUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CrashReport.class)
+public abstract class MixinCrashReport {
+
+    @Shadow
+    private StackTraceElement[] uncategorizedStackTrace;
+
+    @Inject(method = "getDetails(Ljava/lang/StringBuilder;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/CrashReport;details:Ljava/util/List;"))
+    private void addTrace(StringBuilder builder, CallbackInfo ci) {
+        int trailingNewlineCount = 0;
+        if (builder.charAt(builder.length() - 1) == '\n') {
+            builder.deleteCharAt(builder.length() - 1);
+            trailingNewlineCount++;
+        }
+        if (builder.charAt(builder.length() - 1) == '\n') {
+            builder.deleteCharAt(builder.length() - 1);
+            trailingNewlineCount++;
+        }
+        Utils.printTrace(uncategorizedStackTrace, builder);
+        builder.append(StringUtils.repeat("\n", trailingNewlineCount));
+    }
+}
